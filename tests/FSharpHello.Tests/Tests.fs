15428c89262624017d7ceb3036e0101d73ebf4d9
Ondra Pelech <ondra.pelech@gmail.com>
2015-07-13 18:21:07 +0200

    use FsCheck

--- tests/FSharpHello.Tests/Tests.fs
+++ tests/FSharpHello.Tests/Tests.fs
@@ -3,7 +3,12 @@ module FSharpHello.Tests
 open FSharpHello
 open NUnit.Framework
 open FsUnit
+open FsCheck.NUnit
 
 [<Test>]
 let ``hello returns 42`` () =
   Library.hello 42 |> should equal 42
+
+[<Property>]
+let ``hello always returns 42``(x:int) =
+  Library.hello x = 42
